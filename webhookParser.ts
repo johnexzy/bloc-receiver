c971ce13af6ede5d6ea639c698a7887d301bcfc6
John Oba <obajohn75@gmail.com>
2023-09-27 12:40:03 +0100

    bug-fix: added a tryblock

--- webhookParser.ts
+++ webhookParser.ts
@@ -1,17 +1,22 @@
 import axios from "axios";
 
 async function sendWebHook(data: any, url: string) {
-  const options = {
-    url: url,
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-    data: data,
-  };
-
-  await axios(options);
+    try {
+        const options = {
+            url: url,
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Content-Type": "application/json;charset=UTF-8",
+            },
+            data: JSON.stringify(data),
+          };
+        
+          await axios(options);
+    } catch (error) {
+        console.log(error)
+    }
+  
 }
 
 export default sendWebHook;
