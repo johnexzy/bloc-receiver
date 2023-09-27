d2497fdcef4a0e2d112ea2ea7a3ba251320234ab
Johnexzy <obajohn75@gmail.com>
2023-09-27 13:01:10 +0100

    feat: added block sections

--- webhookParser.ts
+++ webhookParser.ts
@@ -1,22 +1,44 @@
 import axios from "axios";
+type trxResponse = {
+  from: string;
+  to: string;
+  value: number;
+  transactionHash: string;
+  transactionType: string;
+  network: string;
+  chainId: number;
+  text?: string;
+  contractAddress: null | string;
+};
 
-async function sendWebHook(data: any, url: string) {
-    try {
-        const options = {
-            url: url,
-            method: "POST",
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json;charset=UTF-8",
+async function sendWebHook(data: trxResponse, url: string) {
+  try {
+    const options = {
+      url: url,
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      data: JSON.stringify({
+        text: data.text,
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "plain_text",
+              text: `${data.from} transferred ${data.value} to ${data.to}`,
+              emoji: true,
             },
-            data: JSON.stringify(data),
-          };
-        
-          await axios(options);
-    } catch (error) {
-        console.log(error)
-    }
-  
+          },
+        ],
+      }),
+    };
+
+    await axios(options);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default sendWebHook;
